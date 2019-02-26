f13b3a4dd0e2c66980b0ac6a51635fc860d8f4ca
Asmin75 <asminrai7@gmail.com>
2019-02-26 10:49:20 +0545

    final change to active menu bar

--- app/views.py
+++ app/views.py
@@ -7,37 +7,49 @@ class HomeView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['active'] = 'home'
 
+        return context
+
 class AboutView(TemplateView):
     template_name = 'app/about-us.html'
 
     def get_context_data(self,**kwargs):
-        context = get_context_data(**kwargs)
+        context = super().get_context_data(**kwargs)
         context['active'] = 'about-us'
+
+        return context
 
 class CourseDetailView(TemplateView):
     template_name = 'app/course-details.html'
 
     def get_context_data(self,**kwargs):
-        context = get_context_data(**kwargs)
-        context['active'] = 'course-details'
+        context = super().get_context_data(**kwargs)
+        context['active'] = 'courses'
+
+        return context
 
 class CourseView(TemplateView):
     template_name = 'app/courses.html'
 
     def get_context_data(self,**kwargs):
-        context = get_context_data(**kwargs)
+        context = super().get_context_data(**kwargs)
         context['active'] = 'courses'
+
+        return context
 
 class ElementView(TemplateView):
     template_name = 'app/elements.html'
 
     def get_context_data(self,**kwargs):
-        context = get_context_data(**kwargs)
+        context = super().get_context_data(**kwargs)
         context['active'] = 'elements'
+
+        return context
 
 class ContactView(TemplateView):
     template_name = 'app/contact.html'
 
     def get_context_data(self,**kwargs):
-        context = get_context_data(**kwargs)
+        context = super().get_context_data(**kwargs)
         context['active'] = 'contact'
+
+        return context
